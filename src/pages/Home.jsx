f7e989c44b12db0e9636a377cac518e15534042a
unknown <sushant.shekhar151997@gmail.com>
2023-08-11 17:54:43 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,28 @@ import WithSubnavigation from '../components/Navbar'
 
 const Home = () => {
   return (
-    <Box backgroundColor={'#274A37'} height={'100vh'} >
+    <Box backgroundColor={'#274A37'} height={'750px'} >
          <WithSubnavigation/>
-         <Grid gridTemplateColumns={{base:'1fr',lg:'10% 44% 44%'}}>
+         <Grid gridTemplateColumns={{base:'1fr',lg:'10% 35% 50%'}}>
           <GridItem>
 
           </GridItem>
           <GridItem >
-            <Image src="./doorpattern.webp" alt="Shivam Enterprise Door pattern" width={'auto'} height={'600px'} />
+            <Image src="./doorpattern.webp" alt="Shivam Enterprise Door pattern" width={'420px'} height={'700px'} />
           </GridItem>
           <GridItem >
-               <Grid gridTemplateRows={'250px 100px 250px'}>
+               <Grid gridTemplateRows={'250px 100px 300px'}>
                 <GridItem  className='SpaceGrotesk'>
-                  <Box  width={'100%'} marginTop={'45px'} >
-                  <Text as={'h1'} fontSize={'70px'} lineHeight={'70px'} textAlign={'left'} fontWeight={'bold'} color={'white'}  >BRINGING BEAUTY AND SECURITY TO YOUR SPACES</Text>
+                  <Box  width={'100%'} marginTop={'100px'} >
+                  <Text as={'h1'} fontSize={'70px'} lineHeight={'70px'} textAlign={'left'} fontWeight={'bold'} color={'white'} >BRINGING BEAUTY AND SECURITY TO YOUR SPACES</Text>
                   </Box>
                 </GridItem>
                 <GridItem >
-                <Box  width={'100%'} marginTop={'10px'} >
-                  <Text as={'h5'} fontSize={'16px'} fontWeight={'bold'} textAlign={'left'}  color={'white'}  >Shivam Enterprises is your one-stop destination for all your UPVC needs. We provide services for UPVC doors, windows, balcony railings and stair railing that are tailor-made to fit your requirements.</Text>
+                <Box  width={'75%'} marginTop={'80px'}  paddingLeft={'5px'} >
+                  <Text as={'h5'} fontSize={'16px'}  textAlign={'left'}  color={'white'}  >Shivam Enterprises is your one-stop destination for all your UPVC needs. We provide services for UPVC doors, windows, balcony railings and stair railing that are tailor-made to fit your requirements.</Text>
                   </Box>
                 </GridItem>
-                <GridItem border={'1px solid yellow'}></GridItem>
+                <GridItem border={'1px solid yellow'} marginTop={'60px'}></GridItem>
                </Grid>
           </GridItem>
          </Grid>
