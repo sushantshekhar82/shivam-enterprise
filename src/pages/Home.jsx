87d155f7b8eaa934f12f88ccae344be62ac99243
unknown <sushant.shekhar151997@gmail.com>
2023-08-23 17:04:36 +0530

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
 
           </GridItem>
           <GridItem >
-            <Image src="./doorpattern.webp" alt="Shivam Enterprise Door pattern" width={'380px'} height={'700px'} />
+            <Image src="./doorpattern.webp" alt="Shivam Enterprise Door pattern" width={{base:'90%',lg:'380px'}} height={'700px'} />
           </GridItem>
           <GridItem >
                <Grid gridTemplateRows={'250px 100px 300px'}>
@@ -38,45 +38,32 @@ const Home = () => {
                </Grid>
           </GridItem>
          </Grid>
+
          <Grid gridTemplateColumns={'70% 28%'}>
-             <GridItem>this is first grid</GridItem>
+             <GridItem padding={'20px'}><Box border={'1px solid red'} height={'300px'}>hi</Box></GridItem>
              <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-              <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem>
-              <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             <GridItem>this is first grid</GridItem>
-             <GridItem>this is second grid</GridItem> 
-             
+            
          </Grid>
+
+         <Box backgroundColor={'#274A37'} width={'100%'} height={'600px'}>
+               <Grid gridTemplateColumns={{base:'1fr',lg:'40% 60%'}}>
+                   <GridItem className='SpaceGrotesk'>
+                    <Box lineHeight={'70px'} padding={'50px'}>
+                      <Text as='h1' fontSize={'70px'} fontWeight={'bold'} color={'white'} textAlign={'left'}>Our Latest</Text>
+                       <Text as='h1' fontSize={'70px'} fontWeight={'bold'} color={'white'} textAlign={'left'}>Projects</Text>
+                   </Box>
+                   </GridItem>
+                    <GridItem className='SpaceGrotesk'>
+                      <Box padding={'50px'}>
+                    <Text as={'h5'} textAlign={'left'}  color={'white'}>Our most recent door and window project was a total makeover of a delightful mid-century modern property in the city's heart. We worked directly with the owner to build custom-made doors and windows that were both functional and elegant in order to enhance the aesthetic of the property while keeping its original character.
+
+The finished product was a magnificent renovation that breathed new life into the house. The owners were overjoyed with the outcome and praised our high level of craftsmanship and attention to detail on the project.
+</Text>
+                      </Box>
+                    </GridItem>
+               </Grid>
+         </Box>
+
     </Box>
   )
 }
